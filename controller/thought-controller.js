5c6ba58e778ee6dcf3f05c86ce02d3eeb0fb60df
DerVerruckte <michael.heinhold@gmail.com>
2021-11-23 14:00:34 -0700

    thought controller

--- controller/thought-controller.js
+++ controller/thought-controller.js
@@ -1,7 +1,79 @@
 const { Thought, User } = require('../models');
 
 const thoughtController = {
-  
+  //find all
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .select('-__v')
+    .then(dbThoughtData => res.json(dbThoughtData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  },
+
+  //find one by id
+  findThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .select('-__v')
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No thought with that ID' });
+        return;
+      }
+      res.json(dbThoughtData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  },
+
+  //create thought (push thought id to associated user)
+  createThought({ body }, res) {
+    Thought.create(body)
+    .then(dbThoughtData => res.json(dbThoughtData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  },
+
+  //update thought
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No thought with that ID' });
+        return;
+      }
+      res.json(dbThoughtData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  },
+
+  //delete thought
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No thought with that ID' });
+        return;
+      }
+      res.json(dbThoughtData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  }
+
+  //add reaction
+
+  //remove reaction
 }
 
 module.exports = thoughtController;
