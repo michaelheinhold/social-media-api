32fe9d72c5f389eb65db9ac86beb3b0d4a94dc94
DerVerruckte <michael.heinhold@gmail.com>
2021-11-23 13:05:35 -0700

    friend controller functions

--- controller/thought-controller.js
+++ controller/thought-controller.js
@@ -1,1 +1,7 @@
 const { Thought, User } = require('../models');
+
+const thoughtController = {
+  
+}
+
+module.exports = thoughtController;
--- controller/user-controller.js
+++ controller/user-controller.js
@@ -77,5 +77,47 @@ const userController = {
       console.log(err);
       res.status(400).json(err);
     });
+  },
+
+  //add friend
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: params.friendId } },
+      { new: true }
+    )
+    .then(dbUserData => {
+      if (!dbUserData){
+        res.status(404).json({ message: 'No user with that ID' });
+        return;
+      }
+      res.json(dbUserData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  },
+
+  //remove friend
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+    .then(dbUserData => {
+      if (!dbUserData){
+        res.status(404).json({ message: 'No user with that ID' });
+        return;
+      }
+      res.json(dbUserData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
   }
 }
+
+module.exports = userController;
